3e073de980ed19d8c9da080c1de9fccda578fe99
miyanokomiya <tomoya.hstexze24632@gmail.com>
2019-05-31 23:01:43 +0900

    add number option

--- src/main.rs
+++ src/main.rs
@@ -7,17 +7,33 @@ use clap::Arg;
 use cron_gate::expression::Expression;
 
 fn main() {
-    let app = app_from_crate!().arg(Arg::from_usage(
-        "<expression> 'Cron Expression: * * * ? * command'",
-    ));
+    let app = app_from_crate!()
+        .arg(Arg::from_usage(
+            "<expression> 'Cron Expression: * * * 7 * command'",
+        ))
+        .arg(
+            Arg::with_name("number")
+                .help("Displayed number")
+                .short("n")
+                .long("number")
+                .takes_value(true)
+                .default_value("10"),
+        );
 
     let matches = app.get_matches();
+
+    let mut number = 10;
+    if let Some(n_str) = matches.value_of("number") {
+        if let Ok(n) = n_str.parse::<usize>() {
+            number = n;
+        }
+    }
 
     if let Some(o) = matches.value_of("expression") {
         match Expression::new(o) {
             Ok(exp) => {
                 let now = Local::now();
-                let datetimes = exp.earler_excuting_datetimes(now, 10);
+                let datetimes = exp.earler_excuting_datetimes(now, number);
                 for (i, dt) in datetimes.iter().enumerate() {
                     println!("{}: {}", i + 1, dt.format("%m/%d %H:%M").to_string());
                 }
